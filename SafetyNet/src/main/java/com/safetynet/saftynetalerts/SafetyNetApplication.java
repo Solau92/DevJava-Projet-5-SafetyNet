3874a02fdbf60276827dd81e250d67cf012e83b1
Sophie LAURENT <sophielaurent92@gmail.com>
2023-02-13 10:28:47 +0100

    New branch develop

--- SafetyNet/src/main/java/com/safetynet/saftynetalerts/SafetyNetApplication.java
+++ SafetyNet/src/main/java/com/safetynet/saftynetalerts/SafetyNetApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SafetyNetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SafetyNetApplication.class, args); 
+		SpringApplication.run(SafetyNetApplication.class, args);
 	}
 
 }
