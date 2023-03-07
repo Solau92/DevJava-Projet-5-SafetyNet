5b3e3bd9c0f3c2777c797f51d7c44a79d4d78e6d
Sophie LAURENT <sophielaurent92@gmail.com>
2023-03-07 09:13:27 +0100

    Actuator

--- SafetyNet/src/test/java/com/safetynet/saftynetalerts/service/PersonServiceTest.java
+++ SafetyNet/src/test/java/com/safetynet/saftynetalerts/service/PersonServiceTest.java
@@ -1,41 +1,69 @@
 package com.safetynet.saftynetalerts.service;
 
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.safetynet.saftynetalerts.exception.PersonNotFoundException;
 import com.safetynet.saftynetalerts.model.Person;
 import com.safetynet.saftynetalerts.repository.PersonsRepository;
-import com.safetynet.saftynetalerts.service.PersonService;
 
 @ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class PersonServiceTest {
-//
-//	private static PersonService personService; 
-//	
-//	@Mock 
-//	private static PersonsRepository persons;
-//	
-//	@Test
-//	void getAllPersonsTest() {
-//				
-////		when(persons.getAllPersons()).thenReturn(new ArrayList<Person>());
-////		
-////		personService = new PersonService();
-////		List<Person> liste = personService.getAllPersons();
-////		
-////		verify(persons, Mockito.times(1)).getAllPersons();
-//
+
+	@Autowired
+	private PersonService personService; 
+	
+	@Mock 
+	private PersonsRepository persons;
+	
+//	@BeforeAll
+//	public void setUp() {
+//		personService = new PersonService(persons);
 //	}
+	
+	@Test
+	void getAllPersons_Succes_Test() throws PersonNotFoundException {
+		
+		//GIVEN 
+		List<Person> listMock = new ArrayList<Person>();
+		Person p = new Person();
+		p.setFirstName("fName");
+		p.setLastName("lName");
+		listMock.add(p);
+		when(persons.getAllPersons()).thenReturn(listMock);
+		
+		//WHEN 
+		List<Person> list = personService.getAllPersons();
+		
+		//THEN 
+		verify(persons, Mockito.times(1)).getAllPersons();
+	}
+	
+	@Test
+	void getAllPersons_NoResult_Test() {
+		
+		//GIVEN 
+		List<Person> listMock = new ArrayList<Person>();
+		when(persons.getAllPersons()).thenReturn(listMock);
+		
+		//WHEN 
+		List<Person> list = persons.getAllPersons();
+		
+		//THEN 
+		verify(persons, Mockito.times(1)).getAllPersons();
+	}
 }
