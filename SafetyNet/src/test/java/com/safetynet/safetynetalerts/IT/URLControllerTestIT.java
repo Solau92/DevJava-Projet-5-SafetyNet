a319ff18acca739f10cb9d08b34930fa1244e807
Sophie LAURENT <sophielaurent92@gmail.com>
2023-03-19 17:50:28 +0100

    Documentation, Jacoco exclude

--- SafetyNet/src/test/java/com/safetynet/safetynetalerts/IT/URLControllerTestIT.java
+++ SafetyNet/src/test/java/com/safetynet/safetynetalerts/IT/URLControllerTestIT.java
@@ -3,16 +3,11 @@ package com.safetynet.safetynetalerts.IT;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.MultiValueMap;
 
 @SpringBootTest
 @AutoConfigureMockMvc
